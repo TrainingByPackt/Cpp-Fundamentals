5e172905f84ee32e64d9032babb0d3db38003f13
Vishal Mewada <vishalm@packt.com>
2019-04-05 17:34:48 +0530

    Create main.cpp

---
+++ Lesson 5/Activity 19/Using Array Header/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    array<int,10> balances;
+    for (int i=0; i < balances.size(); ++i){
+        balances[i] = 0;
+        balances.front() += 100;
+     balances.back() += 100;
+    }
+    
+    std::cout << balances.front() << std::endl;
+    std::cout << balances.back() << std::endl;
+}
