58d585b59309cbf262af6a32439d9510e7ffef96
Vishal Mewada <vishalm@packt.com>
2019-04-05 19:23:27 +0530

    Create main.cpp

---
+++ Lesson 6/Exercise 21: Using Multiple Inheritance to Create a "Welcome to the Community" Message Application/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class DataScienceDev {
+    public:
+        DataScienceDev(){
+            std::cout << "Welcome to the Data Science Developer Community." << std::endl;
+        }
+};
+
+class FutureCppDev {
+    public:
+        FutureCppDev(){
+            std::cout << "Welcome to the C++ Developer Community." << std::endl;
+        }
+};
+
+class Student : public DataScienceDev, public FutureCppDev {
+    public:
+        Student(){
+            std::cout << "Student is a Data Developer and C++ Developer." << std::endl;
+        }
+};
+
+
+int main()
+{
+  Student S1;
+  return 0;  
+}
