35bc3f4597adb9730c8a4c38f9bcb27530345b97
Vishal Mewada <vishalm@packt.com>
2019-04-05 19:21:20 +0530

    Create main.cpp

---
+++ Lesson 6/Exercise 20: Creating a Program to Illustrate Inheritance in C++/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Vehicle {
+    public:
+        int getTankCapacity(){
+            const int tankLiters = 10;
+            std::cout << "The current tank capacity for your car is " << tankLiters << " Liters."<<std::endl;
+            return tankLiters;
+        }
+};
+
+class CollectorItem {
+    public:
+    float getValue() {
+        return 100;
+    }
+};
+
+class Ferrari250GT: protected Vehicle, public CollectorItem {
+    public:
+    Ferrari250GT() {
+        std::cout << "Thank you for buying the Ferrari 250 GT with tank capacity " << getTankCapacity() << std::endl;
+        return 0;
+    }
+};
+
+int main()
+{
+    Ferrari250GT ferrari;
+    std::cout << "The value of the Ferrari is " << ferrari.getValue() << std::endl;
+    /* Cannot call ferrari.getTankCapacity() because Ferrari250GT inherits from Vehicle with the protected specifier */
+    return 0;
+}
